23ad7d8a9eddac03cc1dce3ab62936ffcb858153
juarez <juralima@gmail.com>
2024-03-27 16:37:38 -0300

    Calculo IMC

--- desafio2.js
+++ desafio2.js
@@ -39,10 +39,12 @@ if(imc < 18.5) {
     window.alert(`IMC: ${imc.toFixed(1)}  -- Obesidade grau I`);
 
 } else if(imc >= 35 && imc < 40)  {
-    window.alert(`IMC: ${imc.tofixed(1)}  -- Obesidade grau II`)
+    window.alert(`IMC: ${imc.tofixed(1)}  -- Obesidade grau II`);
 
 } else  {
-    window.alert(`IMC: ${imc.tofixed(1)}   -- Obesidade grau III`)
+    window.alert(`IMC: ${imc.tofixed(1)}   -- Obesidade grau III`);
+
+
 }
 
 
